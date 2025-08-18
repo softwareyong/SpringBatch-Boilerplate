906dc818b7094b03541df05511697aafb312ad86
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2025-08-18 23:53:57 +0900

    feat: jobLauncher 컨트롤러로 실행

---
+++ SampleBatch/src/main/java/com/samplebatch/controller/MainController.java
@@ -0,0 +1,41 @@
+package com.samplebatch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+public class MainController {
+
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    /**
+     * 배치작업이 오래걸리기 때문에 Callable로 비동기 처리하면 좋지만
+     * 일단 다음에 도전
+     * .addString("date", value) 이렇게 파라미터를 받는 이유는 특정 일자에 실행을 하는데
+     * 겹치는 일자가 있다면 중복 실행 방지를 해줌
+     */
+    @GetMapping("/first")
+    public String firstApi(@RequestParam("value") String value) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", value)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+
+
+        return "ok";
+    }
+
+}
+
