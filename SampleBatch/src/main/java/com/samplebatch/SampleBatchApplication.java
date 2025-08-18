516f4dab1958e0eb2570670e38c1d0c574b73502
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2025-08-19 01:42:45 +0900

    feat: 스케쥴 활성화

--- SampleBatch/src/main/java/com/samplebatch/SampleBatchApplication.java
+++ SampleBatch/src/main/java/com/samplebatch/SampleBatchApplication.java
@@ -2,8 +2,10 @@ package com.samplebatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케쥴 활성화하기
 public class SampleBatchApplication {
 
     public static void main(String[] args) {
